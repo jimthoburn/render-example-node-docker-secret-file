e1c14a77ea46f0ee418e66d99b9e0fe5ff26af17
Jim Thoburn <jim@tobbi.co>
2022-08-12 16:41:58 -0700

    Use multiple lines for startup

--- start.sh
+++ start.sh
@@ -5,10 +5,13 @@ set -o allexport
 source ./.secret-env
 set +o allexport
 
+export PRETEND_PRIVATE_KEY=$(cat pretend-private-key)
+
 cat dockerfile-log.txt
 
 echo "Hello from the shell 🐚"
 echo "The value of the SECRET_PORT environment variable is: $SECRET_PORT"
+echo "The value of the PRETEND_PRIVATE_KEY environment variable is: $PRETEND_PRIVATE_KEY"
 
 # Start the application
-export PRETEND_PRIVATE_KEY=$(cat pretend-private-key) && npm start
+npm start
